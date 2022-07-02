9f89eee757190916941b4e7083c813a5f47575b0
Aleisson <caioaleisson@gmail.com>
2022-07-02 19:06:34 -0300

    tela2 - dinamic layout altered

--- script/quiz.js
+++ script/quiz.js
@@ -1,11 +1,20 @@
 
 function expande(element){
     const pergunta = element.parentElement.parentElement;
+    const filhos = pergunta.childElementCount;
+    
     if (!pergunta.classList.contains("expandir")) {
         pergunta.classList.add("expandir");
-        pergunta.style.height = "600px";
+        element.querySelector("ion-icon").style.color = "#FFFFFF";
+        if(filhos === 1){
+            pergunta.style.height = "340px"
+        }else{
+            pergunta.style.height = "740px";
+        }
+        
     } else{
        pergunta.classList.remove("expandir");
-       pergunta.style.height = "64px"
+       pergunta.style.height = "90px"
+       element.querySelector("ion-icon").style.color = "#000000";
     }
 }
